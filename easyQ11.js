e791c8a47cc1e9b11c477c63c0f3043b0c5a7326
Naweed Ahmadi <Naweed-Ahmadi@scusd.edu>
2024-03-25 09:48:12 -0700

    JavaScript Q11 - Easy

---
+++ easyQ11.js
@@ -0,0 +1,13 @@
+// RegEx Exercise 1: Find the Time
+// The time has a format: hours:minutes. Both hours and minutes have two digits, like 09:00.
+
+// Make a regexp to find time in the string: Breakfast at 09:00 in the room 123:456. In this task there’s no need to check time correctness yet, so 25:99 can also be a valid result. The regexp should not match 123:456.
+
+function findTime(str) {
+    let pattern = /\b\d{1,2}:\d{2}\b/g;
+
+    let matches = str.match(pattern)
+    console.log(matches);
+}
+
+findTime('Breakfast at 09:00 in the room 123:456.')
