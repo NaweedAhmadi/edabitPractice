1306c509208fd4d5439d737d8839f8ab421cc084
Naweed Ahmadi <naweed.ahmadi2010@gmail.com>
2024-03-25 13:59:12 -0700

    JavaScript Q12 - Easy

---
+++ easyQ12.js
@@ -0,0 +1,28 @@
+// RegEx XIV: Group Ranges x|y
+
+// Groups and ranges indicate groups and ranges of expression characters. The regular expression x|y matches either "x" or "y".
+
+// const REGEXP = /blue|red/
+
+// "red flag".match(REGEXP)  // red
+// "blue flag".match(REGEXP)  // blue
+
+// // Matches "blue" in "blue flag" and "red" in "red flag".
+
+// Create a regular expression to match all red flag and blue flag in a string. You must use | in your expression. Flags can come in any order.
+
+// Examples
+// "red flag blue flag".match(REGEXP) ➞ ["red flag", "blue flag"]
+
+// "yellow flag red flag blue flag green flag".match(REGEXP) ➞ ["red flag", "blue flag"]
+
+// "pink flag red flag black flag blue flag green flag red flag ".match(REGEXP) ➞ ["red flag", "blue flag", "red flag"]
+
+function regEx(str) {
+    var pattern = /\bblue flag|red flag\b/g;
+
+    var matches = str.match(pattern);
+    console.log(matches)
+}
+
+regEx("pink flag red flag black flag blue flag green flag red flag");
